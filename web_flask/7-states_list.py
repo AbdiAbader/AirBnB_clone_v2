a8df801b473077e4d16987839af79219b1e6a216
AbdiAbader <abdiprocode@gmail.com>
2023-01-15 14:10:01 +0300

    update task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,50 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/')
+def hello():
+    """hello Hbnb returns"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """hello Hbnb returns"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def clove(text):
+    """C is Fun """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def py(text="is cool"):
+    """C is Fun """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    v = "is odd"
+    if n % 2 == 0:
+        v = "is even"
+
+    return render_template('6-number_odd_or_even.html', n=n, v=v)
+    
+
 @app.teardown_appcontext
 def tear_down(self):
     """after each request remove current SQLAlchemy session"""
