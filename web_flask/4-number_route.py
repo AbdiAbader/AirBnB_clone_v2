02356bfb92a4be9587c8b7daf17deb4a1defa877
AbdiAbader <abdiprocode@gmail.com>
2023-01-15 10:12:13 +0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-
-""" Hello Flask """
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:              display "Hello HBNB!"
+            /hbnb:          display "HBNB"
+            /c/<text>:      display "C" + text (replace underscores with space)
+            /python/<text>: display "Python" + text (default is "is cool")
+            /number/<n>:    display "n is a number" only if int
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """hello Hbnb returns"""
-    return 'Hello HBNB!'
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def hello_hbnb():
-    """hello Hbnb returns"""
-    return 'HBNB'
+def hbnb():
+    """display text"""
+    return "HBNB"
 
 
 @app.route('/c/<text>')
-def clove(text):
-    """C is Fun """
+def c_text(text):
+    """display custom text given"""
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
-def py(text="is cool"):
-    """C is Fun """
+def python_text(text="is cool"):
+    """display custom text given
+       first route statement ensures it works for:
+          curl -Ls 0.0.0.0:5000/python ; echo "" | cat -e
+          curl -Ls 0.0.0.0:5000/python/ ; echo "" | cat -e
+    """
     return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
-def num(n):
-    return "{n:d} is a number".format(n)
+def text_if_int(n):
+    """display text only if int given"""
+    return "{:d} is a number".format(n)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
